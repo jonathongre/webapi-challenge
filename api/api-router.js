fa4c130115a97c25b3e2f40f4f5764cc981692ea
Jonathon Green <jonathongre@gmail.com>
2019-09-06 12:04:11 -0500

    Endpoints for projects working

--- api/api-router.js
+++ api/api-router.js
@@ -13,11 +13,44 @@ router.get('/', (req, res) => {
 // Projects
 router.get('/projects', (req, res, next) => {
     Projects.get()
-      .then(projects => {
+        .then(projects => {
         res.status(200).json(projects);
-      })
-      .catch(error => next(error));
-  });
+        })
+        .catch(error => next(error));
+    });
+
+router.post('/projects', (req, res, next) => {
+    Projects.insert(req.body)
+        .then(projects => {
+        res.status(200).json(projects);
+        })
+        .catch(error => next(error));
+    });
+
+router.put('/projects/:id', (req, res) => {
+    const { id } = req.params;
+    const changes = req.body;
+
+    Projects.update(id, changes)
+        .then(updated => {
+        res.status(200).json(updated);
+        })
+        .catch(error => {
+        res.status(500).json(error);
+        });
+    });
+    
+router.delete('/projects/:id', (req, res) => {
+    const { id } = req.params;
+    
+    Projects.remove(id)
+        .then(deleted => {
+        res.status(200).json({ message: `Project with ID ${id} was deleted` });
+        })
+        .catch(error => {
+        res.status(500).json(error);
+        });
+    });
 
 // Actions 
 router.get('/actions', (req, res, next) => {
